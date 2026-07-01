ee539e43c5169b6aaccf26eb2687ea93a40f6c5e
2021180033-LTH <mdk73000@naver.com>
2026-07-01 16:41:24 +0900

    DA 삭제

--- Project/D_A/Source/D_A/D_A.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "D_A.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, D_A, "D_A" );
-
-DEFINE_LOG_CATEGORY(LogD_A)
--- Project/D_A/Source/D_A/D_AGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "D_AGameMode.h"
-
-AD_AGameMode::AD_AGameMode()
-{
-	// stub
-}
